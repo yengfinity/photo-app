e682d36c739ed6ac979241d7c0e28078bb9243ff
Yeng <leeyeng2005@gmail.com>
2017-07-08 10:16:22 -0500

    Add stripe payment

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -2,25 +2,26 @@ $(document).ready(function() {
 
   var show_error, stripeResponseHandler, submitHandler;
 
-  submitHandler = function(event) {
-    var $form = $(event.target);
-    $form.find("input[type=submit]").prop("disabled", true);
+  submitHandler = function (event) {
+     var $form = $(event.target);
+     $form.find("input[type=submit]").prop("disabled", true);
 
-    //If Stripe was initialized correctly this will create a token using the credit card info
-
-    if (Stripe) {
-      Stripe.card.createToken($form, stripeResponseHandler);
-    } else {
-      show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
-    }
-    return false;
+     //If Stripe was initialized correctly this will create a token using the credit card info
+     if(Stripe){
+       Stripe.card.createToken($form, stripeResponseHandler);
+     } else {
+       show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
+     }
+     return false;
   };
 
   $(".cc_form").on('submit', submitHandler);
 
-  stripeResponseHandler = function(status, response) {
+  stripeResponseHandler = function (status, response) {
     var token, $form;
+
     $form = $('.cc_form');
+
     if (response.error) {
       console.log(response.error.message);
       show_error(response.error.message);
@@ -35,16 +36,15 @@ $(document).ready(function() {
       $("[data-stripe=label]").remove();
       $form.get(0).submit();
     }
+
     return false;
   };
-
-  show_error = function(message) {
-    if ($("#flash-messages").size() < 1) {
+  show_error = function (message) {
+    if($("#flash-messages").size() < 1){
       $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
     }
     $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
     $('.alert').delay(5000).fadeOut(3000);
     return false;
   };
-
 });
